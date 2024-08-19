28acb40b7a45886bdbafd6d607d342e12d1b05b4
thekaiofc <kaio.hen13@gmail.com>
2024-08-18 22:23:08 -0300

    Entregando projeto desenvolvido com base nos vídeos do curso de Design patterns da DIO

--- lab-padroes-projeto-spring/src/main/java/dio/lab_padroes_projeto_spring/service/ClienteService.java
+++ lab-padroes-projeto-spring/src/main/java/dio/lab_padroes_projeto_spring/service/ClienteService.java
@@ -2,10 +2,12 @@ package dio.lab_padroes_projeto_spring.service;
 
 import dio.lab_padroes_projeto_spring.model.Cliente;
 
+import java.util.Optional;
+
 public interface ClienteService {
     Iterable<Cliente> buscarTodos();
 
-    Cliente buscarPorId(Long id);
+    Optional<Cliente> buscarPorId(Long id);
 
     void inserir(Cliente cliente);
 
--- lab-padroes-projeto-spring/src/main/java/dio/lab_padroes_projeto_spring/service/impl/ClienteServiceImpl.java
+++ lab-padroes-projeto-spring/src/main/java/dio/lab_padroes_projeto_spring/service/impl/ClienteServiceImpl.java
@@ -1,32 +1,60 @@
 package dio.lab_padroes_projeto_spring.service.impl;
 
 import dio.lab_padroes_projeto_spring.model.Cliente;
+import dio.lab_padroes_projeto_spring.model.ClienteRepository;
+import dio.lab_padroes_projeto_spring.model.Endereco;
+import dio.lab_padroes_projeto_spring.model.EnderecoRepository;
 import dio.lab_padroes_projeto_spring.service.ClienteService;
+import dio.lab_padroes_projeto_spring.service.ViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class ClienteServiceImpl implements ClienteService {
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepService viaCepService;
 
     @Override
     public Iterable<Cliente> buscarTodos() {
-        return null;
+        return clienteRepository.findAll();
     }
 
     @Override
     public Cliente buscarPorId(Long id) {
-        return null;
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.get();
     }
 
     @Override
     public void inserir(Cliente cliente) {
+        salvarClienteComCep(cliente);
+    }
 
+    private void salvarClienteComCep(Cliente cliente) {
+        String cep = cliente.getEndereco().getCep();
+        enderecoRepository.findById(cep).orElseGet(() -> {
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
     }
 
     @Override
     public void atualizar(Long id, Cliente cliente) {
-
+        Optional<Cliente> clienteBd = clienteRepository.findById(id);
+        if (clienteBd.isPresent()){
+            salvarClienteComCep(cliente);
+        }
     }
 
     @Override
     public void deletar(Long id) {
-
+        clienteRepository.deleteById(id);
     }
 }
